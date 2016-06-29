9cc027e6dd8afc68564b43ee26715e92c0e5c216
Michael Lopez <michael@weahead.se>
2016-06-29 20:53:43 +0200

    Allow arbitrary command to run on startup.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [ "${NODE_ENV}" = "development" ]; then
-  echo "Running in development mode."
-  echo "Executing: exec npm run dev"
-  exec npm run dev
-elif [ "${NODE_ENV}" = "production" ]; then
-  echo "Running in production mode."
-  echo "Executing: exec npm start"
-  exec npm start
+CMD="$1"
+
+if [ -z "${CMD}" ]; then
+    if [ "${NODE_ENV}" = "development" ]; then
+        CMD="dev"
+    elif [ "${NODE_ENV}" = "production" ]; then
+        CMD="start"
+    fi
 fi
+
+echo "Running in ${NODE_ENV} mode."
+echo "Executing: exec npm run ${CMD}"
+exec npm run "${CMD}"
